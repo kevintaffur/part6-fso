8545931cfaebab0d12042f9b608bbe64d8616386
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-28 14:16:19 -0500

    exercise 6.8

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux';
 import AnecdoteForm from './components/AnecdoteForm';
-import { addVote } from './reducers/anecdoteReducer';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(addVote(id));
-  };
-
-  const sortByVotes = (anecdote1, anecdote2) => {
-    if (anecdote1.votes > anecdote2.votes) {
-      return -1;
-    }
-    if (anecdote1.votes < anecdote2.votes) {
-      return 1;
-    }
-    return 0;
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort(sortByVotes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addVote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    dispatch(addVote(id));
+  };
+
+  const sortByVotes = (anecdote1, anecdote2) => {
+    if (anecdote1.votes > anecdote2.votes) {
+      return -1;
+    }
+    if (anecdote1.votes < anecdote2.votes) {
+      return 1;
+    }
+    return 0;
+  };
+
+  return (
+    <>
+      {anecdotes.sort(sortByVotes).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AnecdoteList;
